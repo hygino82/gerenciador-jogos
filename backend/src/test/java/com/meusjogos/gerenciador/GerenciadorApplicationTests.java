dc0453c3af6e20a79a08d590592501a87f14549d
Adroaldo <hygino@msn.com>
2022-04-15 14:08:49 -0300

    Projeto criado

---
+++ backend/src/test/java/com/meusjogos/gerenciador/GerenciadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meusjogos.gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
