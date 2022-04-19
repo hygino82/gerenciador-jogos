53d0c08576c8d91c3e78b6facbe519c6df820335
Adroaldo <hygino@msn.com>
2022-04-19 10:23:20 -0300

    Adicionado ModelMapper

--- backend/src/main/java/com/meusjogos/gerenciador/config/Config.java
+++ backend/src/main/java/com/meusjogos/gerenciador/config/Config.java
@@ -2,6 +2,7 @@ package com.meusjogos.gerenciador.config;
 
 import java.util.Collections;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -36,5 +37,10 @@ public class Config {
 
         );
 
+    }
+
+    @Bean
+    private ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 }
